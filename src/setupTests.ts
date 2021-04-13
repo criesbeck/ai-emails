0c874dba0e42de4d795d6edd615d2c14577fa2b6
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-04-13 13:52:30 -0500

    Remove unnecessary files

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,7 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-
 process.env.REACT_APP_SUBMISSION_DATA = "/example-submission-data.json";
 process.env.REACT_APP_AUTHORS = "/authors.json";
 process.env.REACT_APP_POKE = "/poke-325-export.json";
