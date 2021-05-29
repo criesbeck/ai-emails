8c4f4e1dc250b070b0369c44922a687104b89786
Joshua Hoeflich <joshuaharry411@icloud.com>
2021-05-28 19:06:53 -0500

    Add processor

--- src/help-system/tagPostProcessors.ts
+++ src/help-system/tagPostProcessors.ts
@@ -1,8 +1,13 @@
 import { TagPostProcessor } from "./tagStructure";
 import { getDoingFine } from "./utils";
 
-export const aboveZeroProcessor: TagPostProcessor = (globalCtx) =>
-  globalCtx.issues.filter((issue) => issue.weight > 0);
+export const aboveZeroProcessor: TagPostProcessor = ({ issues }) =>
+  issues.filter((issue) => issue.weight > 0);
+
+export const canRelaxProcessor: TagPostProcessor = ({ issues }) => {
+  const doingFine = issues.find((issue) => issue.name === "can_relax");
+  return doingFine ? [doingFine] : issues;
+};
 
 export const doingFineProcessor: TagPostProcessor = (globalCtx) => {
   const { issues, history } = globalCtx;
